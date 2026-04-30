7c9b3b05e1a7c03c17d28bef7efd684b35276b1b
cojoman21 <cojocaru.petrubd@gmail.com>
2026-05-01 00:00:50 +0300

    Add basic calculator functionality Add basic operations and clear function. ! Multiple operations in a row to be added next.

--- scripts.js
+++ scripts.js
@@ -15,9 +15,10 @@ function divide(a, b) {
   //   return a / b;
 }
 
-const firstNumber = 0;
-const secondNumber = 0;
-const operator = "";
+let firstNumber = "";
+let secondNumber = "";
+let currentOperator = "";
+let result = "";
 
 function operate(operator, a, b) {
   switch (operator) {
@@ -33,3 +34,68 @@ function operate(operator, a, b) {
       return;
   }
 }
+
+// document.getElementById("display").value = "0";
+
+const btnDigits = document.querySelectorAll("#digit");
+
+const waitingFirstNumber = () => {
+  if (result === "") return true;
+  return false;
+};
+
+const waitingSecondNumber = () => {
+  if (currentOperator !== "") return true;
+  return false;
+};
+
+btnDigits.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    if (waitingFirstNumber()) {
+      firstNumber += event.target.innerText;
+      document.getElementById("display").value = Math.abs(
+        parseInt(firstNumber),
+      );
+    } else if (waitingSecondNumber()) {
+      secondNumber += event.target.innerText;
+      document.getElementById("display").value = secondNumber;
+    }
+  });
+});
+
+function enterValue(value) {
+  firstNumber += value;
+  document.getElementById("display").value = firstNumber;
+}
+
+const btnOperators = document.querySelectorAll("#operator");
+
+btnOperators.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    if (firstNumber === "") {
+      firstNumber = event.target.innerText;
+    } else if (firstNumber !== "" && currentOperator === "") {
+      currentOperator = event.target.innerText;
+    } else {
+      a = parseInt(firstNumber);
+      b = parseInt(secondNumber);
+      result = operate(currentOperator, a, b);
+      document.getElementById("display").value = result;
+
+      firstNumber = result;
+      secondNumber = "";
+      currentOperator = "";
+    }
+  });
+});
+
+const btnClear = document.querySelector("#clear");
+btnClear.addEventListener("click", () => {
+  document.getElementById("display").value = "0";
+  firstNumber = "";
+  secondNumber = "";
+  currentOperator = "";
+  result = "";
+});
+
+function calculate() {}
